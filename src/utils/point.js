2af955fedc62b2ad29637f262f76971f06a8a606
Alexandr Timofeev <tim31al@gmail.com>
2020-12-03 07:49:04 +0300

    refactoring utils

---
+++ src/utils/point.js
@@ -0,0 +1,32 @@
+import dayjs from "dayjs";
+
+export const getDuration = (startDate, endDate) => {
+  const diffMinutes = dayjs(endDate).diff(startDate, `minute`);
+
+  if (diffMinutes < 60) {
+    return `${printTimeUnit(diffMinutes)}M`;
+  }
+
+  let hours = Math.floor(diffMinutes / 60);
+  if (hours < 24) {
+    const minutes = Math.floor(diffMinutes - hours * 60);
+    return `${printTimeUnit(hours)}H ${printTimeUnit(minutes)}M`;
+  }
+
+  const days = Math.floor(hours / 24);
+  hours = Math.floor(hours - days * 24);
+  const minutes = Math.floor(diffMinutes - hours * 60 - days * 24 * 60);
+
+  return `${printTimeUnit(days)}D ${printTimeUnit(hours)}H ${printTimeUnit(minutes)}M`;
+};
+const printTimeUnit = (unit) => {
+  return unit > 10 ? unit : `0${unit}`;
+};
+export const isPointPast = (endDate) => {
+  return dayjs().isAfter(endDate, `date`);
+};
+export const isPointFuture = (startDate) => {
+  const currentDate = dayjs();
+  return currentDate.isSame(startDate, `date`)
+    || currentDate.isBefore(startDate, `date`);
+};
---
+++ src/utils/render.js
@@ -0,0 +1,66 @@
+import AbstractView from "../view/abstract";
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTER: `after`,
+  BEFORE: `before`,
+};
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+export const render = (container, element, place) => {
+  if (container instanceof AbstractView) {
+    container = container.getElement();
+  }
+
+  if (element instanceof AbstractView) {
+    element = element.getElement();
+  }
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTER:
+      container.after(element);
+      break;
+    case RenderPosition.BEFORE:
+      container.before(element);
+      break;
+  }
+};
+export const replace = (newChild, oldChild) => {
+  if (newChild instanceof AbstractView) {
+    newChild = newChild.getElement();
+  }
+
+  if (oldChild instanceof AbstractView) {
+    oldChild = oldChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || newChild === null) {
+    throw new Error(`Can't replace unexisting elements`);
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+export const remove = (component) => {
+  if (!(component instanceof AbstractView)) {
+    throw new Error(`Can remove only components`);
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};
+
