c397950a620d1a8e5b248e05ff7bda0047c8105f
Alexandr Timofeev <tim31al@gmail.com>
2021-01-11 14:43:08 +0300

    correction 1

--- src/utils/modal-window.js
+++ src/utils/modal-window.js
@@ -4,7 +4,7 @@ coverElement.classList.add(`cover`);
 const messageElement = document.createElement(`div`);
 messageElement.classList.add(`error-message`);
 
-export const modal = (messageHtml) => {
+export const modalWindow = (messageHtml) => {
   messageHtml += `<button class="ok-button">OK</button>`;
   messageElement.innerHTML = messageHtml;
 
